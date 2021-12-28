7a7cdf6c2a68f2c75028215bcd568772910aae53
Fangjun Kuang <csukuangfj@gmail.com>
2021-12-28 21:45:15 +0800

    Remove unused code.

--- optimized_transducer/python/csrc/optimized_transducer.cc
+++ optimized_transducer/python/csrc/optimized_transducer.cc
@@ -15,14 +15,6 @@ PYBIND11_MODULE(_optimized_transducer, m) {
   m.def("compute_transducer_loss", &ComputeTransducerLoss, py::arg("logits"),
         py::arg("targets"), py::arg("logit_lengths"), py::arg("target_lengths"),
         py::arg("blank"));
-
-#if 1
-  // for test only, will remove this block
-  torch::Tensor RowSplitsToRowIds(const torch::Tensor &row_splits,
-                                  int32_t num_elems /*= -1*/);
-  m.def("row_splits_to_row_ids", &RowSplitsToRowIds, py::arg("row_splits"),
-        py::arg("num_elems") = -1);
-#endif
 }
 
 }  // namespace ot
--- optimized_transducer/python/tests/test_cuda.py
+++ optimized_transducer/python/tests/test_cuda.py
@@ -2,28 +2,16 @@
 
 # Copyright      2021  Xiaomi Corporation (authors: Fangjun Kuang)
 
-import _optimized_transducer
 import torch
 
 import optimized_transducer
 
 
-def test_row_splits():
-    size = torch.tensor([1, 5, 0, 2])
-    row_splits = torch.cumsum(size, dim=-1)
-    row_splits = torch.cat([torch.tensor([0]), row_splits])
-    device = torch.device("cuda", 0)
-    row_splits = row_splits.to(torch.int32).to(device)
-    row_ids = _optimized_transducer.row_splits_to_row_ids(row_splits)
-    print(row_ids)
-    row_ids2 = _optimized_transducer.row_splits_to_row_ids(
-        row_splits,
-        row_splits[-1],
-    )
-    print(row_ids2)
-
-
 def test_loss():
+    if torch.cuda.is_available():
+        print("cuda is not available - skipping")
+        return
+
     T1 = 3
     T2 = 4
 
