c3b29a7c4030f5dd19b9d6ada382bd82b00fce9b
Fangjun Kuang <csukuangfj@gmail.com>
2022-06-10 11:23:42 +0800

    Fix installation on macOS. (#34)
    * Fix installation on macOS.
    
    * More fixes.

--- optimized_transducer/python/optimized_transducer/__init__.py
+++ optimized_transducer/python/optimized_transducer/__init__.py
@@ -1,1 +1,11 @@
+import torch
+
+from .torch_version import optimized_transducer_torch_version
+
+if torch.__version__.split("+")[0] != optimized_transducer_torch_version.split("+")[0]:
+    raise ImportError(
+        f"optimized_transducer was built using PyTorch {optimized_transducer_torch_version}\n"
+        f"But you are using PyTorch {torch.__version__} to run it"
+    )
+
 from .transducer_loss import TransducerLoss, transducer_loss  # noqa
